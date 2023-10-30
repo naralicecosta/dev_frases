95a632dde563beae502cb6ed54008efcbfc573e9
naralicecosta <naralicecosta04@gmail.com>
2023-10-29 22:43:13 -0300

    feat: adicionando as categorias

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,34 @@ import './App.css'
 import LogoImg from './assets/logo.png'
 
 function App() {
+  const [textoFrase, setTextoFrase] = useState ("")
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState(0) // sempre começar com a primeira categoria selecionada
+
+  // variavel js com um array de objetos com nome de categoria e as frases
+  const allFrases = [
+    {
+      id: 1,
+      nome: "Motivação",
+      frases: [
+        'siga os bons',
+        'Há ainda tanta coisa linda na vida para se descobrir.',
+        'Que todos os nossos passos nos levem para o que nos faz feliz de verdade.',
+        'Siga sempre confiante do seu potencial!',
+        'Hoje é um novo dia. Não fique pensando no ontem.',
+      ]
+    },
+    {
+      id:2,
+      nome: 'Bom dia',
+      frases:[
+        'Mais um dia começando! Vamos firmes e fortes alcançar nossos objetivos!',
+        'Desejo que este dia seja marcado por novas oportunidades, muito amor e, principalmente, repleto de felicidade. Bom dia',
+        'Agradeça por este dia maravilhoso. Bom dia!',
+        'Desejo a todos um dia iluminado!',
+        'Bom dia para você que acordou com a certeza de que dias melhores virão!',
+      ]
+    }
+  ]
 
   return (
     <div className='container'>
@@ -12,11 +40,18 @@ function App() {
 
       <h2 className='title'>Categorias</h2>
       <section className='category-area'>
-        <button className='category-button'>Motivação</button>
-        <button className='category-button'>Bem estar</button>
+        {allFrases.map((item, index) =>  (// map para percorrer os items
+          <button
+          key={item.id}
+           className='category-button'>
+            {item.nome}
+            </button>
+        ))}
       </section>
       <button className='button-frase'>Gerar frase</button>
-      <p className='textoFrase'>Alguma frase vai vir aqui</p>
+      <p className='textoFrase'>
+        {textoFrase !== '' && <p className='textoFrase'>{textoFrase}</p>}
+      </p>
           
     </div>
   )
